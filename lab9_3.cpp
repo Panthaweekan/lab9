c453fa4d683975de6e906bf1762c76bb7fcfdaa2
Panthaweekan <tatanoobza1234@hotmail.com>
2021-12-07 16:47:32 +0700

    FINISH lab9_3.cpp

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,11 +1,61 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your property: "
-"Your status = "
-"UNFRIEND"
-"FRIEND"
-"BEST FRIEND"
-"ONE-NIGHT-STAND"
-"MARRIED"
-*/
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    int age,Height,Prop;
+    string status;
+    cout << "Enter your age: ";
+    cin >> age;
+    if(age <= 20)
+    {
+        cout << "Enter your height: ";
+        cin >> Height;   
+
+        if(Height < 160)
+        {   
+            status = "UNFRIEND";
+        }
+        else if(Height < 175)
+        {
+            status = "FRIEND";
+        }
+        else
+        {
+    
+           cout << "Enter your property: "; 
+           cin >> Prop;
+
+           if(Prop > 69e6)
+           {
+               status = "MARRIED";
+           }
+           else
+           {
+               status = "ONE-NIGHT-STAND";
+           }
+
+        }
+    }
+    else if (age < 30)
+    {
+        cout << "Enter your property: "; 
+        cin >> Prop;
+        if(Prop > 10e6)
+        {
+            status = "BEST FRIEND";
+        }
+        else
+        {
+            status = "UNFRIEND";
+        }
+    }
+    else
+    {
+        status = "UNFRIEND";
+    }
+
+    cout << "Your status = " << status;
+    
+}
